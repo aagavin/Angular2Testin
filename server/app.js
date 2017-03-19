ff98ae5471819a9101d3114ab11367d1c28afae5
Aaron <aagavin@gmail.com>
2017-03-19 11:14:53 -0400

    express.js

--- server/app.js
+++ server/app.js
@@ -4,12 +4,15 @@ let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
+
 let index = require('./routes/index');
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../client'));
+// app.set('views', path.join(__dirname, '/../client/dist'));
+
+console.log(__dirname+'/../client/dist');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -17,9 +20,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.use('/', index);
+app.use('/api', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 let express = require('express');
+let requests = require('requests');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) =>{
+  res.status(200).json({
+    'success': true
+  })
 });
+
+router.get('/search')
 
 module.exports = router;
