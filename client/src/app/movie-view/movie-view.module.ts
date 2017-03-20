e5bad1c1d24f03969b1776f5e3ed489b54baaf4a
Aaron <aagavin@gmail.com>
2017-03-20 18:50:14 -0400

    movie service

--- client/src/app/movie-view/movie-view.module.ts
+++ client/src/app/movie-view/movie-view.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MovieViewRoutingModule } from './movie-view-routing.module';
 import { MovieViewComponent } from './components/movie-view/movie-view.component';
 
+import { MovieService } from './services/movie.service';
+
 @NgModule({
   imports: [
     CommonModule,
     MovieViewRoutingModule
   ],
   declarations: [MovieViewComponent],
-  bootstrap: [MovieViewComponent]
+  bootstrap: [MovieViewComponent],
+  providers: [MovieService]
 })
 export class MovieViewModule { }
