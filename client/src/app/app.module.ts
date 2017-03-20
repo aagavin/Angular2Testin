23f4082f895ff4752700b97242d38453bbdae6ad
Aaron <aagavin@gmail.com>
2017-03-19 20:41:09 -0400

    view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { MovieSearchModule } from './movie-search/movie-search.module';
-import { MovieViewComponent } from './movie-view/movie-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MovieViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
