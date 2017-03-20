ce4476b6378e783cc217b7b49eff901a71ba6b97
Aaron <aagavin@gmail.com>
2017-03-19 20:35:08 -0400

    error handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,15 +10,19 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/search', (req, res) => {
-  let query = req.query.q;
-  console.log('QUERY: ' + query)
-  request.get(`https://v2.sg.media-imdb.com/suggests/${query[0]}/${query}.json`, (error, response, body)=>{
-    let jsonResponse = body.replace('imdb$logan(', '')
-    jsonResponse = jsonResponse.slice(0, -1);
-    console.log(JSON.parse(jsonResponse))
-    res.status(200).json(JSON.parse(jsonResponse)['d'])
-  })
-  
+  if (req.query.q !== '') {
+    let query = (req.query.q).split(' ').join('_')
+    
+    request.get(`https://v2.sg.media-imdb.com/suggests/${query[0]}/${encodeURIComponent(query)}.json`, (error, response, body) => {
+      let jsonResponse = body.replace(`imdb$${query}(`, '')
+      jsonResponse = jsonResponse.substring(0, jsonResponse.length-1);
+      res.status(200).json(JSON.parse(jsonResponse)['d'])
+    })
+  }
+  else{
+    res.sendStatus(200).json({'error': true})
+  }
+
 })
 
 module.exports = router;
