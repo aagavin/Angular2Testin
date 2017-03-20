0b651e91adaa194de159069b896ece97264aed37
Aaron <aagavin@gmail.com>
2017-03-20 18:50:51 -0400

    filter movies baised on if valid movie or TV

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -12,17 +12,30 @@ router.get('/', (req, res, next) => {
 router.get('/search', (req, res) => {
   if (req.query.q !== '') {
     let query = (req.query.q).split(' ').join('_')
-    
+
     request.get(`https://v2.sg.media-imdb.com/suggests/${query[0]}/${encodeURIComponent(query)}.json`, (error, response, body) => {
       let jsonResponse = body.replace(`imdb$${query}(`, '')
-      jsonResponse = jsonResponse.substring(0, jsonResponse.length-1);
-      res.status(200).json(JSON.parse(jsonResponse)['d'])
+      jsonResponse = jsonResponse.substring(0, jsonResponse.length - 1);
+      
+      res.status(200).json(parseResponse(jsonResponse))
     })
   }
-  else{
-    res.sendStatus(200).json({'error': true})
+  else {
+    res.sendStatus(200).json({ 'error': true })
   }
 
 })
+
+
+let parseResponse = (jsonResponse) => {
+  let validMovies = []
+  j = JSON.parse(jsonResponse)
+  j['d'].forEach((value) => {
+    if (typeof value['q'] !== 'undefined') {
+      validMovies.push(value)
+    }
+  })
+  return validMovies
+}
 
 module.exports = router;
